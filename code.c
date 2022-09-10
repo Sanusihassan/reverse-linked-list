7bf93b0e99510a603c05f2bad4f83cd9d816ba39
Sanusi Hassan <60857931+Sanusihassan@users.noreply.github.com>
2022-09-11 01:45:59 +0200

    Create code.c

---
+++ code.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int data;
+    struct node *next;
+} node;
+
+node *createNode(int val)
+{
+    node *newNode = malloc(sizeof(node));
+    newNode->data = val;
+    newNode->next = NULL;
+    return newNode;
+}
+void insert(node **head, int data)
+{
+    node *newnode = createNode(data);
+    if ((*head) == NULL)
+        (*head) = newnode;
+    else
+    {
+        node *tmp = (*head);
+        while (tmp->next)
+        {
+            tmp = tmp->next;
+        }
+        tmp->next = newnode;
+    }
+}
+void printList(node *head)
+{
+    node *tmp = head;
+    while (tmp != NULL)
+    {
+        printf("%d ", tmp->data);
+        tmp = tmp->next;
+    }
+}
+node *reversed = NULL;
+void reverse(node **head)
+{
+    if ((*head) == NULL)
+    {
+        // insert(&reversed, (*head)->data);
+        return;
+    }
+    reverse(&((*head)->next));
+    insert(&reversed, (*head)->data);
+}
+int main()
+{
+    node *head = NULL;
+    insert(&head, 0);
+    insert(&head, 1);
+    insert(&head, 2);
+    insert(&head, 3);
+    insert(&head, 4);
+    printList(head);
+    reverse(&head);
+    puts("");
+    printList(reversed);
+    return 0;
+}
